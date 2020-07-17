2928bcbdfc3ca670156f7a2c0c3ba9b84d8105d1
Aaron Wilson <adub65@gmail.com>
2020-07-17 13:36:12 -0400

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,9 @@
+// Functional Expression
+let divide = function() {
+  return 2000 / 100
+}
+
+// Arrow Expression
+let square = num1 => num1 ** 2
+
+let add = (parameter1, parameter2) => parameter1 + parameter2
